96a5b0d87ad06a2b242d43bf9c04596336a5b5aa
Uri Shaked <uri@urishaked.com>
2020-12-29 13:14:28 +0200

    fix: migrate redirects to netlify format

--- next.config.js
+++ next.config.js
@@ -1,11 +1,2 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/youtube',
-        destination: `https://www.youtube.com/channel/UC9TfgtLt0500BXqPVVmfuTA`,
-        permanent: true,
-      },
-    ];
-  },
 };
