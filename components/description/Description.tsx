f06d36b5805fda9f7bfd28a93b570e532cef65e7
Nisan Ayash <nisanayash93l@gmaile.com>
2021-03-09 18:03:32 +0200

    add a paragraph to description section

--- components/description/Description.tsx
+++ components/description/Description.tsx
@@ -4,11 +4,22 @@ import style from './description.module.scss';
 const Description = () => {
   return (
     <div className={style.container}>
-      <h3>
-        <span className={style.brandColor}>Pull Request</span> עוזרת לכם להיכנס לפרוייקטי קוד פתוח
-        ולתרום להם ע"י <br />
-        יצירת סביבה תומכת ובטוחה וחיבור עם מנטורים בעלי ניסיון שאכפת להם שתצליחו
-      </h3>
+      <div>
+        <h3>
+          <span className={style.brandColor}>Pull Request</span> עוזרת לכם להיכנס לפרוייקטי קוד פתוח
+          ולתרום להם ע"י <br />
+          יצירת סביבה תומכת ובטוחה וחיבור עם מנטורים בעלי ניסיון שאכפת להם שתצליחו
+        </h3>
+        <p className={style.subDesc}>
+          מוזמנים לתרום לפרויקט של הקהילה או לפרויקטים שרצים תחתיה בסדנאות שרצות אחת לחודש לתרומה
+          לקוד פתוח.
+          <br /> בנוסף מוזמנים להצטרף להרצאות העשרה, לשתף שאלות ובקשות לעזרה ולשתף מהידע שלכם בקבוצה
+          שלנו בפייסבוק.
+          <br />
+          אם אתם רוצים להצטרף כמנטורים של פרויקט ,<br /> מוזמנים למלא את הטופס הבא:
+          https://forms.gle/6mHgTs5PnsfXUZ1f8
+        </p>
+      </div>
     </div>
   );
 };
