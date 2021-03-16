cc334d4ae904a15ed8668a9b6b48c05a50173a69
Nisan Ayash <nisanayash93l@gmaile.com>
2021-03-16 18:57:02 +0200

    add description comp

---
+++ components/description/Description.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './description.module.scss';
+
+const Description = () => {
+  return <div className={style.container}>hey</div>;
+};
+
+export default Description;
