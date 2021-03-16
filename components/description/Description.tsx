edfe3cbfc61efde5bdaaa8648c8fb76cd71e6708
Nisan Ayash <nisanayash93l@gmaile.com>
2021-03-16 19:42:04 +0200

    add description

--- components/description/Description.tsx
+++ components/description/Description.tsx
@@ -7,10 +7,19 @@ const Description = () => {
     <div className={style.container}>
       <div className={style.content}>
         <h3 className={style.title}>
-          <span style={{ color: colors.secondary_color }}>Request Pull</span> עוזרת לכם להיכנס
-          לפרוייקטי קוד פתוח ולתרום להם ע"י <br />
+          <span style={{ color: colors.secondary_color, fontWeight: 'bolder' }}>Request Pull</span>{' '}
+          עוזרת לכם להיכנס לפרוייקטי קוד פתוח ולתרום להם ע"י <br />
           יצירת סביבה תומכת ובטוחה וחיבור עם מנטורים בעלי ניסיון שאכפת להם שתצליחו
         </h3>
+        <p className={style.description}>
+          מוזמנים לתרום לפרויקט של הקהילה או לפרויקטים שרצים תחתיה בסדנאות שרצות אחת לחודש לתרומה
+          לקוד פתוח.
+          <br /> בנוסף מוזמנים להצטרף להרצאות העשרה, לשתף שאלות ובקשות לעזרה ולשתף מהידע שלכם בקבוצה
+          שלנו בפייסבוק.
+          <br />
+          אם אתם רוצים להצטרף כמנטורים של פרויקט,
+          <br /> מוזמנים למלא את הטופס הבא: https://forms.gle/6mHgTs5PnsfXUZ1f8
+        </p>
       </div>
       <div className={style.contributions}>b</div>
     </div>
