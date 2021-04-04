ea3f0c88b87f11397752a995228e0ccc13f2b369
Nisan Ayash <nisanayash93@gmail.com>
2021-04-04 23:28:58 +0300

    fix container positioning &  basic squares created

--- components/description/Description.tsx
+++ components/description/Description.tsx
@@ -20,8 +20,45 @@ const Description = () => {
           https://forms.gle/6mHgTs5PnsfXUZ1f8
         </p>
       </div>
+      <ul className={style.squareContainer}>
+        <li className={style.one} id="1"></li>
+        <li className={style.two} id="2"></li>
+        <li className={style.three} id="3"></li>
+        <li className={style.four} id="4"></li>
+        <li className={style.five} id="5"></li>
+      </ul>
     </div>
   );
 };
 
 export default Description;
+
+<div class="content">
+  <h1 class="title">
+    Using SCSS's <code>@each</code> directive
+  </h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam ex doloribus exercitationem
+    saepe voluptas recusandae,{' '}
+    <a class="articles" href="#">
+      soluta
+    </a>
+    , ab eaque repellat eius alias! Doloribus, libero neque aperiam atque odit repudiandae tenetur
+    facere accusamus, explicabo vero optio id.{' '}
+    <a class="youtube" href="#">
+      Consectetur fuga
+    </a>{' '}
+    ut, in ipsa illo eos vero fugiat quam dignissimos sint voluptas deleniti facilis reiciendis,
+    placeat praesentium nemo magnam tenetur? Aspernatur dolorum cum quisquam labore consectetur,
+    molestiae eius in voluptas repudiandae quas perspiciatis,{' '}
+    <a class="community" href="#">
+      maxime a veritatis
+    </a>{' '}
+    dolor ratione velit suscipit dignissimos. Suscipit rerum atque{' '}
+    <a class="courses" href="#">
+      numquam quisquam corrupti
+    </a>
+    , esse autem modi velit repellendus, dolorem nostrum consequuntur soluta nesciunt animi
+    recusandae temporibus laborum optio vero cum.
+  </p>
+</div>;
