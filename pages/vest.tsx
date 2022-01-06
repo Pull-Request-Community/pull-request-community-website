b68f0deb679722f341a5360f411e73dc59cfa9c1
InbarDanieli <you@example.com>
2022-01-06 12:34:03 +0200

    changes for pr

---
+++ pages/vest.tsx
@@ -0,0 +1,16 @@
+import next from 'next';
+import React from 'react';
+import Navbar from '../components/layout/navbar/navbar';
+import style from '../styles/projects.module.scss';
+
+export default function vast() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <h3 className={style.title}>
+        <div className={style.star} />
+        vest
+      </h3>
+    </React.Fragment>
+  );
+}
