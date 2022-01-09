03f8ecd5e7b7bbc6aad56d2f0e1a79d854bf52b4
InbarDanieli <you@example.com>
2022-01-09 13:47:06 +0200

    Change fregment

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -5,12 +5,12 @@ import style from '../styles/projects.module.scss';
 
 export default function vast() {
   return (
-    <React.Fragment>
+    <>
       <Navbar />
       <h3 className={style.title}>
         <div className={style.star} />
         vest
       </h3>
-    </React.Fragment>
+    </>
   );
 }
