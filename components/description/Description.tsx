db3974699178b99096c16b9cdba8d629da5c07f7
InbarDanieli <you@example.com>
2022-01-10 11:29:24 +0200

    expand description into different components

--- components/description/Description.tsx
+++ components/description/Description.tsx
@@ -2,26 +2,10 @@ import React from 'react';
 import style from './description.module.scss';
 import colors from '../../styles/colors';
 
-const Description = () => {
+const Description = (props) => {
   return (
     <div className={style.container}>
-      <div>
-        <h3>
-          <span style={{ color: colors.secondary_color }}>Pull Request</span> עוזרת לכם להיכנס
-          לפרוייקטי קוד פתוח ולתרום להם ע"י יצירת סביבה תומכת ובטוחה וחיבור עם מנטורים בעלי ניסיון
-          שאכפת להם שתצליחו
-        </h3>
-        <p className={style.subDesc}>
-          מוזמנים לתרום לפרויקט של הקהילה או לפרויקטים שרצים תחתיה בסדנאות שרצות אחת לחודש לתרומה
-          לקוד פתוח.
-          <br /> בנוסף מוזמנים להצטרף להרצאות העשרה, לשתף שאלות ובקשות לעזרה ולשתף מהידע שלכם בקבוצה
-          שלנו בפייסבוק.
-          <br />
-          אם אתם רוצים להצטרף כמנטורים של פרויקט,
-          <br /> מוזמנים למלא את הטופס הבא:
-          <a href="https://forms.gle/6mHgTs5PnsfXUZ1f8">✍</a>
-        </p>
-      </div>
+      {props.descriptionOutput}
       <ul className={style.squareContainer}>
         {Array.from({ length: 5 }).map((_, idx) => (
           <li key={idx} />
