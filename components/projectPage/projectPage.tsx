b7c13a1e6f484f088450397aaaeaf53e9200f85c
InbarDanieli <you@example.com>
2022-01-13 14:57:25 +0200

    PR fixes

--- components/projectPage/projectPage.tsx
+++ components/projectPage/projectPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Languages from './languages/Languages';
 import style from './ProjectPage.module.scss';
 
 function ProjectsPage() {
@@ -6,9 +7,9 @@ function ProjectsPage() {
     <div className={style.descriptionContainer}>
       <h3 className={style.title}>
         <div className={style.star} />
-        vest
+        Vest
       </h3>
-      <p className={style.description}>
+      <p dir="auto" className={style.description}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
         labore et dolore magna aliqua. Ac orci phasellus egestas tellus rutrum tellus pellentesque
         eu tincidunt. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc.
@@ -16,11 +17,11 @@ function ProjectsPage() {
         Libero volutpat sed cras ornare arcu dui vivamus. Quisque egestas diam in arcu cursus
         euismod. Egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam. In egestas erat
         imperdiet sed euismod nisi porta. Gravida in fermentum et sollicitudin ac orci phasellus
-        egestas tellus.
+        egestas tellus.{' '}
       </p>
 
       <div className={style.infoContainer}>
-        <div className={style.languages}>html</div>
+        <Languages />
         <div className={style.license}>Mozilla Public License 2.0</div>
       </div>
     </div>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,11 +4,14 @@ import Description from '../components/description/Description';
 import Navbar from '../components/layout/navbar/navbar';
 import style from '../styles/projects.module.scss';
 import ProjectPage from '../components/projectPage/projectPage';
+import Footer from '../components/layout/footer/footer';
+import Layout from '../components/layout/layout';
 export default function vast() {
   return (
     <>
       <Navbar />
       <Description descriptionOutput={<ProjectPage />} />
+      <Footer />
     </>
   );
 }
