2785737c835f633ce235f0b60402a158a0a00b05
“bargalya” <galyab@wix.com>
2022-06-06 18:43:09 +0300

    commit merge

---
+++ components/language/Translate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useTranslator } from './useTranslator';
+import style from './translate.module.scss';
+
+interface TranslateProps {
+  translationKey: string;
+}
+
+const Translate = ({ translationKey }) => {
+  return <span className={style.breakLine}> {useTranslator(translationKey)} </span>;
+};
+
+export default Translate;
--- components/main-page-description/MainPageDescription.tsx
+++ components/main-page-description/MainPageDescription.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import style from './MainPageDescription.module.scss';
-import { useTranslator } from '../../language/useTranslator';
+import Translate from '../language/Translate';
 
 function MainPageDescription() {
   return (
     <>
       <h3>
-        <span className={style.title}>{useTranslator('main_page.title')}</span>
-
-        {useTranslator('main_page.description')}
+        <span className={style.title}>
+          <Translate translationKey={'main_page.title'} />
+        </span>
+        <Translate translationKey={'main_page.description'} />
       </h3>
       <p className={style.subDesc}>
-        {useTranslator('main_page.contribute_desc.text1')}
-        <br />
-        {useTranslator('main_page.contribute_desc.text2')}
-        <br />
-        {useTranslator('main_page.join_as_mentors.text1')}
-        <br />
-        {useTranslator('main_page.join_as_mentors.text2')}
-        <a
-          href="https://forms.gle/6mHgTs5PnsfXUZ1f8"
-          target="_blanck"
-          title={useTranslator('main_page.mentors_signup')}
-        >
+        <Translate translationKey={'main_page.contribute'} />
+        <a href="https://forms.gle/6mHgTs5PnsfXUZ1f8" target="_blanck">
           ✍
         </a>
       </p>
