0a8c3026af8e29dc91ec17df7d2d7db920b9914e
“bargalya” <galyab@wix.com>
2022-06-06 18:43:28 +0300

    commit merge

--- components/main-page-description/MainPageDescription.tsx
+++ components/main-page-description/MainPageDescription.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import style from './MainPageDescription.module.scss';
-import Translate from '../language/Translate';
+import { useTranslator } from '../language/useTranslator';
 
 function MainPageDescription() {
   return (
     <>
       <h3>
-        <span className={style.title}>
-          <Translate translationKey={'main_page.title'} />
-        </span>
-        <Translate translationKey={'main_page.description'} />
+        <span className={style.title}>{useTranslator('main_page.title')}</span>
+        <span className={style.breakLine}>{useTranslator('main_page.description')}</span>
       </h3>
-      <p className={style.subDesc}>
-        <Translate translationKey={'main_page.contribute'} />
+      <p className={style.breakLine}>
+        {useTranslator('main_page.contribute')}
         <a href="https://forms.gle/6mHgTs5PnsfXUZ1f8" target="_blanck">
           ✍
         </a>
