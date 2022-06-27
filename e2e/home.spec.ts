2d517cff44810691e4de3ded7fa039029b280c98
Liran Tal <liran.tal@gmail.com>
2022-06-27 20:09:22 +0300

    chore: added line break because

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 
+
 test('page title should have the value of "קהילת Pull Request"', async ({page}) => {
   await page.goto('http://localhost:3000/');
   const title = await page.title();
   expect(title).toBe('קהילת Pull Request');
-});
+})
