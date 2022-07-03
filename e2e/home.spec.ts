5a0139fe249b84c721e0096bf62ad1bdf1c7fff7
Omri Bar-Zik <omri@bar-zik.com>
2022-07-03 21:41:10 +0300

    chore: fix lint

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,8 +1,7 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
-test('page should have title of "קהילת Pull Request"', async ({page}) => {
-    await page.goto('http://localhost:3000/')
-    const title = await page.title()
-    expect(title).toBe('קהילת Pull Request')
+test('page should have title of "קהילת Pull Request"', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  const title = await page.title();
+  expect(title).toBe('קהילת Pull Request');
 });
-
