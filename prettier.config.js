e64dff3272fbfdb217a8efe54ab2ee0d48c1c56c
Omri Bar-Zik <omri@bar-zik.com>
2022-07-03 19:16:29 +0300

    build: align editorconfig & prettier configs

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,11 @@
+/** @type {import('prettier').Config} */
 module.exports = {
   arrowParens: 'always',
   printWidth: 100,
   singleQuote: true,
   tabWidth: 2,
-  endOfLine: 'auto',
+  endOfLine: 'lf',
+  useTabs: false,
   overrides: [
     {
       files: '*.scss',
