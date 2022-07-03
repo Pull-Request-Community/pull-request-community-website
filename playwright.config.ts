c2e9525eac435208927e42f39b20f8bd9c8a4202
Omri Bar-Zik <omri@bar-zik.com>
2022-07-03 21:41:10 +0300

    chore: fix lint

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,10 @@
-import { PlaywrightTestConfig } from "@playwright/test";
+import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-    webServer: {
-        command: "npm run start",
-        url: "http://localhost:3000",
-    }
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:3000',
+  },
 };
 
 export default config;
