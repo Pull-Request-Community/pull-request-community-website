a2d61cddf42add8cdbe84b889d9d4b886c05758f
Liran Tal <liran.tal@gmail.com>
2022-07-08 10:54:56 +0300

    test: explicitly use the baseURL variable

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
-test('page should have title of "קהילת Pull Request"', async ({page}) => {
-    await page.goto('./')
+test('page should have title of "קהילת Pull Request"', async ({page, baseURL}) => {
+    await page.goto(baseURL)
     const title = await page.title()
     expect(title).toBe('קהילת Pull Request')
 });
