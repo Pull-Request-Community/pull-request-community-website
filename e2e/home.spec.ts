c7ac57a031d7b7358f9946a57c18351689d2ec0a
Liran Tal <liran.tal@gmail.com>
2022-07-08 10:42:08 +0300

    test: should use relative url notation

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('page should have title of "קהילת Pull Request"', async ({page}) => {
-    await page.goto('/')
+    await page.goto('./')
     const title = await page.title()
     expect(title).toBe('קהילת Pull Request')
 });
