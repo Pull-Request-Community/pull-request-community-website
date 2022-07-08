96eb55b36f722ef80a87103c1f3703773eccf2f0
Liran Tal <liran.tal@gmail.com>
2022-07-08 10:34:03 +0300

    test: now we can fix URL to be relative

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('page should have title of "קהילת Pull Request"', async ({page}) => {
-    await page.goto('http://localhost:3000/')
+    await page.goto('/')
     const title = await page.title()
     expect(title).toBe('קהילת Pull Request')
 });
