b3ae129e5f7db25e6adaac986f39a2bc6cd7f0dc
Liran Tal <liran.tal@gmail.com>
2022-07-08 10:03:14 +0300

    chore: run a test on the deployed netlify url

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,9 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+    use: {
+        baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000'
+    },
     webServer: {
         command: "npm run start",
         url: "http://localhost:3000",
