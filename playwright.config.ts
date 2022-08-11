a14739d927ad7131a0c642d05e11c9d88e7073da
Liran Tal <liran.tal@gmail.com>
2022-08-11 10:40:03 +0300

    test: resolve conflicts for e2e tracing

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,13 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000',
-  },
+    webServer: {
+        command: "npm run start",
+        url: "http://localhost:3000",
+    },
+    use: {
+        trace: 'on',
+    }
 };
 
 export default config;
