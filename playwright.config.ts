005a671293ea09ad7321985235acf7bed0ab20f0
Liran Tal <liran.tal@gmail.com>
2022-08-23 16:23:12 +0000

    fix: lint formatting

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,13 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-    use: {
-        baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000'
-    },
-    webServer: {
-        command: "npm run start",
-        url: "http://localhost:3000",
-    }
+  use: {
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000',
+  },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:3000',
+  },
 };
 
 export default config;
