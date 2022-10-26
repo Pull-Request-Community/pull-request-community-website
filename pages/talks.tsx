0fd89667a2595ebca005741a98b316b46b61a9ff
Rina <rina.oksman@vonage.com>
2022-10-26 21:51:58 +0300

    use translator

--- pages/talks.tsx
+++ pages/talks.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import TalksPage from '../components/talksPage/talksPage';
 import style from '../styles/talks.module.scss';
 import Layout from '../components/layout/layout';
+import languageFile from '../components/talksPage/talksPage.language.json';
+import { useTranslator } from '../components/language/useTranslator';
 
 export default function Vast() {
-  return <Layout descriptionContent={
-    <div className={style.descriptionContainer}>
-      <h3 className={style.title}>Talks</h3>
-      <p dir="auto" className={style.description}>Pull Request is the biggest open-source community in israel with more then 4.5K developers.</p>
-    </div>}>
-    <TalksPage />
-  </Layout>;
+  return (
+    <Layout
+      descriptionContent={
+        <div className={style.descriptionContainer}>
+          <h3 className={style.title}>{useTranslator('title', languageFile)}</h3>
+          <p dir="auto" className={style.description}>
+            {useTranslator('description', languageFile)}
+          </p>
+        </div>
+      }
+    >
+      <TalksPage />
+    </Layout>
+  );
 }
